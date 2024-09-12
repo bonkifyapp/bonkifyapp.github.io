56de285788dd6ea53b2059866fa6562451abe436
jamalvh <jamalvh@umich.edu>
2024-09-11 22:43:21 -0400

    added boring and okay

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3780705602"
+    serviceWorkerVersion: "2274586989"
   }
 });
